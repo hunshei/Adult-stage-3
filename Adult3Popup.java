671e23abb891e90c5c5e963a07686d9185b76284
hunshei <humairaasharol@gmail.com>
2024-07-20 06:14:34 +0300

    Update Adult3Popup.java
    made iscompleted thingy

--- Adult3Popup.java
+++ Adult3Popup.java
@@ -4,6 +4,7 @@ import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.shape.Rectangle;
@@ -22,27 +23,49 @@ public class Adult3Popup implements Initializable {
     private ImageView take, care, of, yourself;
 
     @FXML
+    private Label completeLabel;
+
+    @FXML
     void handleDragDone(DragEvent event) {
     }
 
     private double mouseAnchorX, mouseAnchorY;
 
-    Drag drag = new Drag();
+    Drag drag1 = new Drag();
+    Drag drag2 = new Drag();
+    Drag drag3 = new Drag();
+    Drag drag4 = new Drag();
+
 
     public void initialize(URL location, ResourceBundle resources) {
-        drag.makeDraggableSnap(take,shadow1);
-        drag.makeDraggableSnap(care,shadow2);
-        drag.makeDraggableSnap(of, shadow3);
-        drag.makeDraggableSnap(yourself, shadow4);
+        drag1.makeDraggableSnap(take,shadow1);
+        drag2.makeDraggableSnap(care,shadow2);
+        drag3.makeDraggableSnap(of, shadow3);
+        drag4.makeDraggableSnap(yourself, shadow4);
 
-        drag.makeDraggable(are);
-        drag.makeDraggable(cat);
-        drag.makeDraggable(duck);
-        drag.makeDraggable(empty);
-        drag.makeDraggable(excel);
-        drag.makeDraggable(heart);
-        drag.makeDraggable(how);
-        drag.makeDraggable(meeting);
+        drag1.makeDraggable(are);
+        drag1.makeDraggable(cat);
+        drag1.makeDraggable(duck);
+        drag1.makeDraggable(empty);
+        drag1.makeDraggable(excel);
+        drag1.makeDraggable(heart);
+        drag1.makeDraggable(how);
+        drag1.makeDraggable(meeting);
+
+        startCheckingCompletion();
+    }
+    private void startCheckingCompletion() {
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                stageCompleted();
+            }
+        }.start();
+    }
+
+    public void stageCompleted() {
+        if (drag1.isSnap && drag2.isSnap && drag3.isSnap && drag4.isSnap) {
+            completeLabel.setVisible(true);
+        }
     }
 }
-
